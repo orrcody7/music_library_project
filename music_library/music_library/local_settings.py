2a1f87674b561621b94bcb20ab1c409553a03c56
unknown <orrcody7@gmail.com>
2021-09-02 17:20:09 -0400

    installed django framework, installed rest_framework, created app, and created local settings, also created a Song model to start testing with Postman

---
+++ music_library/music_library/local_settings.py
@@ -0,0 +1,15 @@
+SECRET_KEY = 'django-insecure-5afj!u^g@ldnq!-jpsa6ge+z&ka77^y81e^h!rvs612q7gpcl^'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'music_library',
+        'USER': 'root',
+        'PASSWORD': 'Captain1992!!!!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
